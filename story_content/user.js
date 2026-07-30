2c265de047261d60bb2c31c96ac41cf1471c394e
smartstudygrid <amankhan@smartstudygrid.com>
2026-07-30 21:08:17 +0300

    30 july

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,17 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6EBEXqUxbTl":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  var inputs = document.querySelectorAll('input');
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].type = 'password';
+}
 }
 
