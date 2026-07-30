0d0453719663bb93f2b530d0b33661ac708f6ccf
smartstudygrid <amankhan@smartstudygrid.com>
2026-07-31 02:07:16 +0300

    ee

--- story_content/user.js
+++ story_content/user.js
@@ -2,13 +2,45 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5xtLbkULtfr":
+      case "6pyGLLEqQJi":
         Script1();
+        break;
+      case "5q8QkQP3eWZ":
+        Script2();
         break;
   }
 }
 
 function Script1()
+{
+  var elem = document.documentElement;
+
+if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+  // Enter Fullscreen
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+  }
+} else {
+  // Exit Fullscreen
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
+}
+
+function Script2()
 {
   var inputs = document.querySelectorAll('input');
 for (var i = 0; i < inputs.length; i++) {
